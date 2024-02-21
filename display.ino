85820df82bae5e3dae6a2a42419f7d46ffd8864b
mdingucsb <148507143+mdingucsb@users.noreply.github.com>
2024-02-20 19:12:29 -0800

    Create display.ino

---
+++ display.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define OLED_RESET -1  // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  // detects if error occurs
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+
+  // clears display for new content
+  display.clearDisplay();
+
+  // draws empty game board
+  display.drawRect(37, 5, 52, 52);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
