e610271d5b7957222e56d9a71dbb48b372ea6181
MeliO231 <160688843+MeliO231@users.noreply.github.com>
2024-02-23 17:17:51 -0800

    Update joystick.ino

--- joystick.ino
+++ joystick.ino
@@ -2,7 +2,8 @@
 #define VRY_PIN  A1 // Arduino pin connected to VRY pin
 
 int xValue = 0; // To store value of the X axis
-int yValue = 0; // To store value of the Y axis
+int yValue = 0; // To store value of the Y 
+String direction;
 
 void setup() {
   Serial.begin(9600) ;
@@ -15,9 +16,30 @@ void loop() {
   yValue =  -1 * (yValue - 511) + 512;
 
   // print data to Serial Monitor on Arduino IDE
-  Serial.print("x = ");
+  Serial.print(" x = ");
   Serial.print(xValue);
   Serial.print(", y = ");
   Serial.println(yValue);
+
+  // convert analog readings to direction
+  if (xValue < 406 && 213 < yValue < 813)
+  {
+    direction = "left";
+  }
+  else if (xValue > 606 && 213 < yValue < 813)
+  {
+    direction = "right";
+  }
+  else if (yValue < 413 && 206 < xValue < 806)
+  {
+    direction = "down";
+  }
+  else if (yValue > 613 && 206 < xValue < 806)
+  {
+    direction = "up";
+  }
+
+  Serial.print("direction: ");
+  Serial.print(direction);
   delay(200);
 }
