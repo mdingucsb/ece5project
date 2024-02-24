85ec697f7752a4edaba8e5dda0829968515ece79
MeliO231 <160688843+MeliO231@users.noreply.github.com>
2024-02-23 16:35:32 -0800

    Update joystick.ino

--- joystick.ino
+++ joystick.ino
@@ -1,15 +1,3 @@
-int xValue = analogRead(A0);
-int yValue = analogRead(A1);
-
-
-/*
- * Created by ArduinoGetStarted.com
- *
- * This example code is in the public domain
- *
- * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-joystick
- */
-
 #define VRX_PIN  A0 // Arduino pin connected to VRX pin
 #define VRY_PIN  A1 // Arduino pin connected to VRY pin
 
@@ -24,6 +12,7 @@ void loop() {
   // read analog X and Y analog values
   xValue = analogRead(VRX_PIN);
   yValue = analogRead(VRY_PIN);
+  yValue =  -1 * (yValue - 511) + 512;
 
   // print data to Serial Monitor on Arduino IDE
   Serial.print("x = ");
@@ -32,106 +21,3 @@ void loop() {
   Serial.println(yValue);
   delay(200);
 }
-/*
- * Created by ArduinoGetStarted.com
- *
- * This example code is in the public domain
- *
- * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-joystick
- */
-
-#include <ezButton.h>
-
-#define VRX_PIN  A0 // Arduino pin connected to VRX pin
-#define VRY_PIN  A1 // Arduino pin connected to VRY pin
-#define SW_PIN   2  // Arduino pin connected to SW  pin
-
-ezButton button(SW_PIN);
-
-int xValue = 0; // To store value of the X axis
-int yValue = 0; // To store value of the Y axis
-int bValue = 0; // To store value of the button
-
-void setup() {
-  Serial.begin(9600) ;
-  button.setDebounceTime(50); // set debounce time to 50 milliseconds
-}
-
-void loop() {
-  button.loop(); // MUST call the loop() function first
-
-  // read analog X and Y analog values
-  xValue = analogRead(VRX_PIN);
-  yValue = analogRead(VRY_PIN);
-
-  // Read the button value
-  bValue = button.getState();
-
-  if (button.isPressed()) {
-    Serial.println("The button is pressed");
-    // TODO do something here
-  }
-
-  if (button.isReleased()) {
-    Serial.println("The button is released");
-    // TODO do something here
-  }
-
-  // print data to Serial Monitor on Arduino IDE
-  Serial.print("x = ");
-  Serial.print(xValue);
-  Serial.print(", y = ");
-  Serial.print(yValue);
-  Serial.print(" : button = ");
-  Serial.println(bValue);
-}
-/*
- * Created by ArduinoGetStarted.com
- *
- * This example code is in the public domain
- *
- * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-joystick
- */
-
-#define VRX_PIN  A0 // Arduino pin connected to VRX pin
-#define VRY_PIN  A1 // Arduino pin connected to VRY pin
-
-#define LEFT_THRESHOLD  400
-#define RIGHT_THRESHOLD 800
-#define UP_THRESHOLD    400
-#define DOWN_THRESHOLD  800
-
-#define COMMAND_NO     0x00
-#define COMMAND_LEFT   0x01
-#define COMMAND_RIGHT  0x02
-#define COMMAND_UP     0x04
-#define COMMAND_DOWN   0x08
-
-int xValue = 0 ; // To store value of the X axis
-int yValue = 0 ; // To store value of the Y axis
-int command = COMMAND_NO;
-
-void setup() {
-  Serial.begin(9600) ;
-}
-
-void loop() {
-  // read analog X and Y analog values
-  xValue = analogRead(VRX_PIN);
-  yValue = analogRead(VRY_PIN);
-
-  // converts the analog value to commands
-  // reset commands
-  command = COMMAND_NO;
-
-  // check left/right commands
-  if (xValue < LEFT_THRESHOLD)
-    command = command | COMMAND_LEFT;
-  else if (xValue > RIGHT_THRESHOLD)
-    command = command | COMMAND_RIGHT;
-
-  // check up/down commands
-  if (yValue < UP_THRESHOLD)
-    command = command | COMMAND_UP;
-  else if (yValue > DOWN_THRESHOLD)
-    command = command | COMMAND_DOWN;
